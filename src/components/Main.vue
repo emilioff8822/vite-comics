5cd0f7ced6a9feb896263d752753cbcd88f9e362
emilioff8822 <emilioff@hotmail.it>
2023-04-28 13:19:50 +0200

    commit-main-jumbo-card

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,45 @@
-
-
 <script>
 export default {
-name: "Main"
+  name: "Main"
 }
 </script>
 
 <template>
-  <div class="container_blue ht-300">
-    <h1>content goes here</h1>
+  <div class="container">
+    <div class="jumbotron">
+      <img class="logo" src="../assets/img/jumbotron.jpg" alt="logo">
+    </div>
+    <div class="card-container">
+      <!-- Contenuto della card container -->
+      
+    </div>
   </div>
-  
 </template>
 
 <style lang="scss">
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
 
+.jumbotron {
+  background-image: url('../assets/img/jumbotron.jpg');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card-container {
+  background-color: black;
+  height: 60%;
+}
+
+.logo {
+  width: 100%;
+}
 </style>
