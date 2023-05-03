54f180d5b9b5969d9a67ef67c2065bd113c717f8
emilioff8822 <emilioff@hotmail.it>
2023-05-03 13:39:04 +0200

    css-card-props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -36,7 +36,8 @@ export default {
 
 <style scoped>
 .card {
-  background-color: #fff;
+  background-color: #000;
+  color: #fff;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   border-radius: 4px;
   overflow: hidden;
